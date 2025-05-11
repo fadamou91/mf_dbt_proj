f0290093850d2072f25feb5ed6b73935376cac89
fadamou <fayaladamoumoussa@gmail.com>
2025-05-11 23:51:49 +0200

    changes on model sql

--- models/0_staging/customer_stg.sql
+++ models/0_staging/customer_stg.sql
@@ -1,6 +1,6 @@
 select *
 from (
-      select *, row_number() over (partition by customer_id order by load_date des) as rownum
-      from {{ source('landing','tbs_customers')}}
+      select *, row_number() over (partition by customer_id order by dat_load desc) as rownum
+      from {{ source('landing','tbs_customers') }}
       )
 where rownum = 1
